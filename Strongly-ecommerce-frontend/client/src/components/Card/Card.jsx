54f77ad360689f60123a9cb4eeaccc92b978c036
Nicolás D <96535022+Nico2430@users.noreply.github.com>
2025-11-22 13:50:53 -0300

    Mejorado un poco el formato de "Vista" de los productos para ver las fotos

--- Strongly-ecommerce-frontend/client/src/components/Card/Card.jsx
+++ Strongly-ecommerce-frontend/client/src/components/Card/Card.jsx
@@ -4,7 +4,6 @@ export default function Card({ nombre, descripcion, precio }) {
   return (
     <div className="card">
       <h3 className="card-title">{nombre}</h3>
-      <p className="card-description">{descripcion}</p>
       <p className="card-price">{precio}</p>
     </div>
   );
--- Strongly-ecommerce-frontend/client/src/components/FCard/FCard.jsx
+++ Strongly-ecommerce-frontend/client/src/components/FCard/FCard.jsx
@@ -4,7 +4,6 @@ export default function FeaturedCard({ nombre, descripcion, precio }) {
   return (
     <div className="FCard">
       <h3 className="FCard-title">{nombre}</h3>
-      <p className="FCard-description">{descripcion}</p>
       <p className="FCard-price">{precio}</p>
     </div>
   );
